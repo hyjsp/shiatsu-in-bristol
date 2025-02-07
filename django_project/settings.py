0b949e3af008c9f74184dfca40ab3d69a726d5ea
hyjsp <45848870+hyjsp@users.noreply.github.com>
2025-02-07 11:39:03 +0000

    adjusted allowed hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://shiatsu-in-bristol.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'shiatsu-in-bristol.onrender.com']
 
 
 # Application definition
